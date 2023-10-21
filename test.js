15681d371f5fab590f47a8e3c75cf2d748feaeed
Emin.exe <75854763+eminexedev@users.noreply.github.com>
2023-10-21 14:34:53 +0300

    Add files via upload

---
+++ test.js
@@ -0,0 +1,10 @@
+function openCookieContainer() {
+    var container = document.getElementById('cookieContainer');
+    container.style.display = 'block';
+  }
+  
+  function closeCookieContainer() {
+    var container = document.getElementById('cookieContainer');
+    container.style.display = 'none';
+  }
+  
